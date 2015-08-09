458411e89e80bc4a6882744da38c329895fabec3
lorin19881101 <18523010772@163.com>
2015-08-09 17:19:17 +0800

    primary
    primary

--- demo/scripts/primary.js
+++ demo/scripts/primary.js
@@ -17,6 +17,7 @@
     var Primary = function () {};
     Primary.prototype.initialize = function () {
         this.downloadImgHover();
+        this.animate();
     };
 
     Primary.prototype.downloadImgHover = function () {
@@ -37,6 +38,69 @@
                 $(this).attr('src', 'style/img/Googleplay-download.png');
             }
         );
+    };
+
+    Primary.prototype.animate = function () {
+        function animation () {
+            if ( $('.primary-bottom .song').offset().top < document.documentElement.clientHeight +$(document).scrollTop() - 100) {
+                $('.primary .primary-bottom .content .song .song-img').css({
+                    visibility: 'visible',
+                    animation: 'song-img 1s linear 0s 1',
+                    msAnimation: 'song-img 1s linear 0s 1',
+                    webkitAnimation: 'song-img 1s linear 0s 1',
+                    mozAnimation: 'song-img 1s linear 0s 1',
+                    oAnimation: 'song-img 1s linear 0s 1'
+                });
+
+                $('.primary .primary-bottom .content .song .tip-img').css({
+                    visibility: 'visible',
+                    animation: 'tip-right 2s linear 0s 1',
+                    msAnimation: 'tip-right 2s linear 0s 1',
+                    webkitAnimation: 'tip-right 2s linear 0s 1',
+                    mozAnimation: 'tip-right 2s linear 0s 1',
+                    oAnimation: 'tip-right 2s linear 0s 1'
+                });
+
+                $('.primary .primary-bottom .content .song .text').css({
+                    visibility: 'visible',
+                    animation: 'text-right 2.2s linear 0s 1',
+                    msAnimation: 'text-right 2.2s linear 0s 1',
+                    webkitAnimation: 'text-right 2.2s linear 0s 1',
+                    mozAnimation: 'text-right 2.2s linear 0s 1',
+                    oAnimation: 'text-right 2.2s linear 0s 1'
+                });
+            }
+
+            if ( $('.primary-bottom .social-song').offset().top < document.documentElement.clientHeight + $(document).scrollTop() - 100) {
+                $('.primary .primary-bottom .content .social-song .new-people-entry').css({
+                    visibility: 'visible',
+                    animation: 'new-people-entry 1s linear 0s 1',
+                    msAnimation: 'new-people-entry 1s linear 0s 1',
+                    webkitAnimation: 'new-people-entry 1s linear 0s 1',
+                    mozAnimation: 'new-people-entry 1s linear 0s 1',
+                    oAnimation: 'new-people-entry 1s linear 0s 1'
+                });
+
+                $('.primary .primary-bottom .content .social-song .tip-img').css({
+                    visibility: 'visible',
+                    animation: 'tip-left 2s linear 0s 1',
+                    msAnimation: 'tip-left 2s linear 0s 1',
+                    webkitAnimation: 'tip-left 2s linear 0s 1',
+                    mozAnimation: 'tip-left 2s linear 0s 1',
+                    oAnimation: 'tip-left 2s linear 0s 1'
+                });
+
+                $('.primary .primary-bottom .content .social-song .text').css({
+                    visibility: 'visible',
+                    animation: 'text-left 2.2s linear 0s 1',
+                    msAnimation: 'text-left 2.2s linear 0s 1',
+                    webkitAnimation: 'text-left 2.2s linear 0s 1',
+                    mozAnimation: 'text-left 2.2s linear 0s 1',
+                    oAnimation: 'text-left 2.2s linear 0s 1'
+                });
+            }
+        };
+        $(window).on('scroll', animation);
     };
 
     var primaryObj = new Primary();
